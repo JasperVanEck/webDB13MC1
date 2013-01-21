a820d06c2db194cf639a3b408a9d3e78baeafad0
Thomas van Til <tomvantil@gmail.com>
2013-01-21 14:32:32 +0100

    update products

--- deleteproducts.php
+++ deleteproducts.php
@@ -1,5 +1,6 @@
 <?php
 
 //Scriptje om bij de product zelf, zodra je ingelogd bent als admin, een delete button toe te voegen die het product uit de database verwijdert.
+//Zelfde admin login check line als bij addproducts
 
 ?>
