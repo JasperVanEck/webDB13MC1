c32e74303ce02083dc9c3f2e45a5c5f67ca47de6
Thomas van Til <tomvantil@gmail.com>
2013-01-21 11:36:56 +0100

    Submit bestanden
    & STyle.css update

---
+++ subscribe.php
@@ -0,0 +1,22 @@
+<?php
+
+$con = mysql_connect("https://webdb.science.uva.nl/webdb13MC1/","webdb13MC1","renunaca") or die('Could not connect: ' . mysql_error());
+
+mysql_select_db("Newsletter", $con);
+
+if(mysql_query("INSERT INTO Newsletter (email) VALUES (".$_POST['email']."')")) {
+
+echo "Your email has been added to our list!";
+
+} else {
+
+echo "There was an error adding your email to our list. Please try again.";
+
+}
+
+mysql_close($con);
+
+?>
+
+
+
