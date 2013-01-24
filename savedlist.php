fa177dcb1b5a2a78d5341218dd1202a9e47c6e43
timothydingeman <timothydingeman@gmail.com>
2013-01-24 16:22:17 +0100

    bla

--- savedlist.php
+++ savedlist.php
@@ -6,16 +6,17 @@ echo "<div id='main'>";
 $totalprice = null;
 
 try {
-	//Connect to Databse
+	//Connect to Database
 	$dbh = connect();
 
     	$user_id = 2;//$_SESSION["myuid"];
 
-	$sql = "SELECT Product_id, Amount FROM Content WHERE User_id = $user_id AND Ordered_bool=0";
+	$sql = "SELECT Product_id, Amount FROM Content WHERE User_id = $user_id AND Saved=1";
 	$product_id = $dbh->query($sql);
 	echo "<table id='includebag'>";
 
-	echo "<tr>
+	echo "<tr>	
+			<td> </td></br>
                        	<td> <strong> Name <strong> </td></br>
                        	<td> <strong> Price <strong></td></br>
                        	<td> <strong> Size <strong></td></br>
@@ -36,28 +37,32 @@ try {
 		$color = null;
 		
 		foreach($product_result as $i)	
-		{
+		{	
+			$foto_id = $i['Foto_id'];
 			$name = $i["Name"];
 			$price = $i["Price"];
 			$size = $i["Size"];
 			$color = $i["Color"];
 		}
 		$subtotal = $price * $amount;
-		$totalprice += $price * $amount;
+		
 		echo "<tr>
+			<td><div>
+      			<img src='$foto_id' alt='$name' class='preview'>
+			</div></td></br>
 			<td> $name </td></br>
-			<td> &#8364 $price </td></br>
+			<td> $price </td></br>
 			<td> $size </td></br>
 			<td> $color </td></br>
 			<td> $amount </td></br>
-			<td> &#8364 $subtotal </td></br>
-		</tr>";
+			<td> $subtotal </td></br>
+			</tr>";
 	
 	}
 
 	echo "</table>"; 
        	echo "<table id='totalprice'>";
-      	echo "<tr><td><strong> TOTAL PRICE: &#8364 $totalprice <strong></td>";
+	echo "<tr><td><strong> TOTAL PRICE: &#8364 $totalprice <strong></td>";
    	echo '<td><form action="#payment_url" method="post">
                                	<input type="submit" value="PAY" class="paybagbutton"$
               </form></td></tr>
@@ -67,9 +72,9 @@ try {
 	
 	$dbh = null;
 }
-catch (PDOException $e) {
-	    print "Error!: " . $e->getMessage() . "<br/>";
-	    die();
+	catch (PDOException $e) {
+	print "Error!: " . $e->getMessage() . "<br/>";
+	die();
 }
 echo "</div>";
 ?>
